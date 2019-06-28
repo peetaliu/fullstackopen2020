aee2e08b336e5c012d32737178a1d52b0540e129
Peter Liu <peterdotliu@gmail.com>
2019-06-28 18:06:51 +0000

    Submission for exercise 1.2 Course Information Step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,17 +4,18 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
     return (
         <div>
-        <h1>{props.course}</h1>
+            <h1>{props.course}</h1>
         </div>
-
     )
 }
 
 const Content = (props) => {
     return (
         <div>
-            <p>{props.part} {props.excercise}</p>
-            </div>
+            <Part coName="Fundamentals of React" numEx="10"/>
+            <Part coName="Using props to pass data" numEx="7"/>  
+            <Part coName="State of a component" numEx="14"/>  
+        </div>
     )
 }
 
@@ -26,21 +27,27 @@ const Total = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <div>
+            <p>{props.coName} {props.numEx}</p>
+        </div>
+    )
+}
+
 const App = () => {
-    const course = 'Half Stack application developement';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
+    const course = 'Half Stack application development'
+    //const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    //const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    //const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
         <div>
             <Header course={course}/>
-            <Content part={part1} exercise={exercises1}/>
-            <Content part={part2} exercise={exercises2}/>
-            <Content part={part3} exercise={exercises3}/>
+            <Content />
             <Total exCount={exercises1 + exercises2 + exercises3}/>
         </div>
     )
