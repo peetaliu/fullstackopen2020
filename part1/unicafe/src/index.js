840ca19323696729a9209e6ee1dd9e1f189993b7
Peter Liu <peter_liu930@hotmail.com>
2019-11-29 04:49:11 +0000

    Submit 1.6: unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,26 +1,35 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-//BOOKMARK: PART1D 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const App = (props) => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <div>
-        {left}
-        <button onClick={() => setLeft(left + 1)}>
-          left
-        </button>
-        <button onClick={() => setRight(right + 1)}>
-          right
-        </button>
-        {right}
-      </div>
+      <h1>give feedback</h1>
+      <Button handleClick={()=> setGood(good + 1)} text="good" />
+      <Button handleClick={()=> setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={()=> setBad(bad + 1)} text="bad" />
+
+      <h1>statistics</h1>
+
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p> 
+
     </div>
   )
 }
 
-ReactDOM.render( < App /> , document.getElementById('root'));
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
