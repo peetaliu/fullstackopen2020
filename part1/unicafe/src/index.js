335e346db42d629d325af304cc5f715b94faab66
Peter <peterdotliu@gmail.com>
2019-11-29 05:20:31 +0000

    Submit 1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,6 +13,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  function calculateAvg(pos, neu, neg){
+    let avg = (pos-neg)/(pos+neu+neg)
+
+    return avg
+  }
+
+  function getPosPer(g, n, b){
+    return (g/(g+n+b))*100
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -25,7 +35,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p> 
-
+      <p>all {good + neutral + bad}</p>
+      <p>average {calculateAvg(good, neutral, bad)}</p>
+      <p>positive {getPosPer(good, neutral, bad)} %</p>
     </div>
   )
 }
