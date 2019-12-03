d37b178f2cf7bafc88bf252889b342b9afa41830
Peter Liu <peterdotliu@gmail.com>
2019-12-03 22:55:28 +0000

    Submit 1.13:anecdotes step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -9,13 +9,25 @@ const Button = (props) => (
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-
-  const ranNum = Math.floor(Math.random()*anecdotes.length)
+  // eslint-disable-next-line
+  const [votes, setVotes] = useState([])
+  
+  const getVote = (item) => {
+    let counter = 0
+    for (const vote of votes){
+      if(vote === item){
+        counter ++
+      }
+    }
+    return counter
+  }
 
   return (
     <div>
       <p>{props.anecdotes[selected]}</p>
-      <Button handleClick={()=>setSelected(ranNum)} text="Next anecdote"/>
+      <p>has {getVote(selected)} votes</p>
+      <Button handleClick={()=>setSelected(Math.floor(Math.random()*props.anecdotes.length))} text="Next anecdote"/><br/>
+      <Button handleClick={()=>setVotes(votes.concat(selected))} text="Vote"/>
     </div>
     
   )
