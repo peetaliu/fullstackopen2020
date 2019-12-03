af238c18804e1285aa5cb069039c84e36e8ee48d
Peter Liu <peter_liu930@hotmail.com>
2019-12-03 19:11:07 +0000

    Submit 1.11:unicafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,33 +8,29 @@ const Button = (props) => (
 )
 
 const Statistics = (props) => {
-  const {title, g, n, b} = props
-  if(!g && !n && !b){
-    return(
-      <div>
-        <h1>{title}</h1>
-        <p>No feedback given</p>
-      </div>
-    )
-  } else {
-    return (
-    <div>
-      <h1>{title}</h1>
-      <p>good {g}</p>
-      <p>neutral {n}</p>
-      <p>bad {b}</p>
-      <p>all {g + n + b} </p>
-      <p>avg {(g-b)/(g+n+b)}</p>
-      <p>positive {(g/(g+n+b))*100}</p>
-    </div>
-    )
+  const {text, value} = props
+  const chkValue = (v) => {
+    if(!v){
+      return 0
+    } else {
+      return value
+    }
   }
+  return(
+    <tr>
+      <td>
+        {text} {chkValue(value)}
+      </td>
+    </tr>
+  )
 }
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
 
   return (
     <div>
@@ -42,9 +38,17 @@ const App = () => {
       <Button handleClick={()=> setGood(good + 1)} text="good" />
       <Button handleClick={()=> setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={()=> setBad(bad + 1)} text="bad" />
-
-      <Statistics title="Statistics" g={good} n={neutral} b={bad}/>
-      
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <Statistics text="good" value={good}/>
+          <Statistics text="neutral" value={neutral}/>
+          <Statistics text="bad" value={bad}/>
+          <Statistics text="all" value={good+neutral+bad}/>
+          <Statistics text="average" value={(good-bad)/(good+neutral+bad)}/>
+          <Statistics text="positive" value={(good/(good+neutral+bad))*100}/>
+        </tbody>
+      </table>
     </div>
   )
 }
