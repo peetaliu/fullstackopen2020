cf02ea4d91a533729393a0e1732d8c70f476ab60
Peter Liu <peter_liu930@hotmail.com>
2019-12-12 23:00:07 +0000

    2.1 in progress

--- part2/coursecontents/src/index.js
+++ part2/coursecontents/src/index.js
@@ -1,12 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+// eslint-disable-next-line
+const Content = (props) => {
+    let partArr = props.parts;
+
+    return (
+        <div>
+            <Part coName={partArr[0].name} numEx={partArr[0].exercises}/>
+            <Part coName={partArr[1].name} numEx={partArr[1].exercises}/>  
+            <Part coName={partArr[2].name} numEx={partArr[2].exercises}/>  
+        </div>
+    )
+}
+// eslint-disable-next-line
+const Total = (props) => {
+    let ex = props.parts
+    const sum = (p1, p2, p3) => {
+        return p1 + p2 + p3
+    }
+    let totalCount = sum(ex[0].exercises, ex[1].exercises, ex[2].exercises);
+    return (
+        <div>
+            <p>Number of exercises {totalCount}</p>
+        </div>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            <p>{props.coName} {props.numEx}</p> 
+        </div>
+    )
+}
+
+const Course = ({course}) => {
+  
+  // const courseNames = () => {
+  //   course.parts.map(a => <Part coName = {a.name}/>)
+  // }
+  return (
+  <div>
+    <Header title = {course.name}/>
+    {course.parts.map(a => <Part coName = {a.name}/>)}
+  </div>
+)
+
+  }
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
