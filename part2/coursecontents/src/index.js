32073cf7a2c5b89e4d6163e5d43fe3176f968843
Peter Liu <peter_liu930@hotmail.com>
2019-12-13 23:27:06 +0000

    Add function to render arbitrary amount of courses. TOFIX: Unique Key in Course component map function

--- part2/coursecontents/src/index.js
+++ part2/coursecontents/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+// eslint-disable-next-line
 const Header = (props) => {
     return (
         <div>
@@ -8,7 +8,6 @@ const Header = (props) => {
         </div>
     )
 }
-
 
 const Content = (props) => {
     let partArr = props.parts;
@@ -44,48 +43,88 @@ const Part = (props) => {
     )
 }
 
-const Course = ({course}) => {
-  let exCount = course.parts.map(x => x.exercises)
-  //console.log('excount', exCount);
+const Course = ({courses}) => {
   return (
   <div>
-    <Header title = {course.name}/>
-    <Content parts = {course.parts}/>
-    <Total parts = {course.parts}/>
+    {courses.map(cArr => ([<Header title={cArr.name} key={cArr.id}/>, <Content parts={cArr.parts} key={cArr.parts.id}/>, <Total parts={cArr.parts} key={cArr.parts.id}/>]))}
   </div>
 )
 
   }
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  // const course = {
+  //   name: 'Half Stack application development',
+  //   parts: [
+  //     {
+  //       name: 'Fundamentals of React',
+  //       exercises: 10,
+  //       id: 1
+  //     },
+  //     {
+  //       name: 'Using props to pass data',
+  //       exercises: 7,
+  //       id: 2
+  //     },
+  //     {
+  //       name: 'State of a component',
+  //       exercises: 14,
+  //       id: 3
+  //     },
+  //     {
+  //       name: 'Redux',
+  //       exercises: 11,
+  //       id: 4
+  //     }
+  //   ]
+  // }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses} />
     </div>
   )
 }
