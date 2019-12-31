e9b427bd1dff9c1b130d7477f3a97b421ba328c6
Peter <peterdotliu@gmail.com>
2019-12-30 20:06:01 -0500

    Submit 2.8: The Phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,12 @@ import Person from './components/Person'
 const App = () => {
   const [persons, setPersons] = useState([
     { 
-        name: 'Arto Hellas' 
+        name: 'Arto Hellas',
+        number: '416-123-1234'
     }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const showNames = () => persons.map(p =>
         <Person 
@@ -19,21 +21,32 @@ const App = () => {
       e.preventDefault()
 
       const allNames = () => persons.map(p => p.name.toUpperCase())
-      
-      if(allNames().includes(newName.trim().toUpperCase())){
-        window.alert(`${newName} is already in the phonebook. Please use a different name.`)
+      const allNum = () => persons.map(p => p.number)
+      if(!newName.trim() || !newNumber.trim()){
+        window.alert('Text boxes cannot be empty')
       } else {
-        const personObj = {
-          name: newName
-        }
-        setPersons(persons.concat(personObj))
-        setNewName('')
-      }
+        if(allNames().includes(newName.trim().toUpperCase())){
+          window.alert(`${newName} already exists in the phonebook. Please use a different name.`)
+        } else if(allNum().includes(newNumber.trim())){
+          window.alert(`${newNumber} already exists in the phonebook. Please add a different number`)
+        } else {
+          const personObj = {
+            name: newName,
+            number: newNumber
+          }
+          setPersons(persons.concat(personObj))
+          setNewName('')
+          setNewNumber('')
+      }}
   }
 
   const handleNameChange = (e) => {
-      //console.log(e.target.value)
-      setNewName(e.target.value)
+    //console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+  const handleNumChange = (e) => {
+    //console.log(e.target.value)
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -42,6 +55,9 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumChange}/>
         </div>
         <div>
           <button type="submit">add</button>
