58107dcb655080bf01cfa9097ef8ec678bd99e78
Peter <peterdotliu@gmail.com>
2019-12-30 19:25:38 -0500

    Submit 2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,69 +1,8 @@
-// import React, { useState } from 'react'
-// import Note from './components/Note'
-
-// const App = (props) => {
-//   const [notes, setNotes] = useState(props.notes)
-//   const [newNote, setNewNote] = useState('a new note...')
-//   const [showAll, setShowAll] = useState(true)
-
-//   const notesToShow = showAll
-//     ? notes
-//     : notes.filter(note => note.important)
-
-//   const rows = () => notesToShow.map(note =>
-//     <Note
-//       key={note.id}
-//       note={note}
-//     />
-//   )
-
-//   const addNote = (e) => {
-//       e.preventDefault()
-//       const noteObject = {
-//           content: newNote,
-//           date: new Date().toISOString(),
-//           important: Math.random() > 0.5,
-//           id: notes.length + 1,
-//       }
-
-//       setNotes(notes.concat(noteObject))
-//       setNewNote('')
-//   }
-
-//   const handleNoteChange = (e) => {
-//       console.log(e.target.value)
-//       setNewNote(e.target.value)
-//   }
-
-//   return (
-//     <div>
-//       <h1>Notes</h1>
-//       <div>
-//           <button onClick={()=>setShowAll(!showAll)}>
-//               show {showAll ? 'important' : 'all'}
-//           </button>
-//       </div>
-//       <ul>
-//         {rows()}
-//       </ul>
-//       <form onSubmit={addNote}>
-//           <input 
-//             value={newNote}
-//             onChange={handleNoteChange}
-//           />
-//           <button type="submit">save</button>
-//       </form>
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
+  const [persons, setPersons] = useState([
     { 
         name: 'Arto Hellas' 
     }
@@ -78,11 +17,18 @@ const App = () => {
   )
   const addName = (e) => {
       e.preventDefault()
-      const personObj = {
-        name: newName
+
+      const allNames = () => persons.map(p => p.name.toUpperCase())
+      
+      if(allNames().includes(newName.trim().toUpperCase())){
+        window.alert(`${newName} is already in the phonebook. Please use a different name.`)
+      } else {
+        const personObj = {
+          name: newName
+        }
+        setPersons(persons.concat(personObj))
+        setNewName('')
       }
-      setPersons(persons.concat(personObj))
-      setNewName('')
   }
 
   const handleNameChange = (e) => {
