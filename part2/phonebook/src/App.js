f1a98c563f9c5a1ad50d4e728164b851798118ae
Peter <peterdotliu@gmail.com>
2020-01-04 21:45:54 -0500

    Submit 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
-
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect: ', persons)
+    axios
+      .get('http://localhost:3001/persons').then(res => {
+        console.log('promise fulfilled')
+        setPersons(res.data)
+        console.log(`Res Data: ${res.data}`)
+      })
+  }, [])
 
   const handleNameChange = (e) => {
     //console.log(e.target.value)
@@ -34,7 +39,6 @@ const App = () => {
     }
     setSearch(e.target.value)
   }
-
 
   return (
     <div>
