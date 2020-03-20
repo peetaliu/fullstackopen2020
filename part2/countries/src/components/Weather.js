e616d2b0be0ba3b9158150ffca65fd3d85e43cb2
Peter <peterdotliu@gmail.com>
2020-03-20 11:00:35 -0400

    Submit 2.14:Data for Countries, step3

--- part2/countries/src/components/Weather.js
+++ part2/countries/src/components/Weather.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Weather = (props) => {
     const details = props.weather 
-    console.log(details);
+    const conditions = details.weather[0]
+    const temp = details.main.temp
+    const wind = details.wind
+    const getWindSpeed = () => `${(wind.speed*3600)/1000} km/h`
+    const icon = `http://openweathermap.org/img/wn/${conditions.icon}@2x.png`
     return(
-        <h2>Weather in hello</h2>
+        <div>
+            <h2>Weather in {details.name}</h2>
+            <p><strong>Temperature: </strong>{temp} Celcius</p>
+            <img alt={conditions.description} src={icon}></img>
+            <p><strong>Wind: </strong>{getWindSpeed()} {wind.deg} deg</p>
+        </div>
     )
 }
 
