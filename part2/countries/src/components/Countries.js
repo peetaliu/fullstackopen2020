22b3560d9369f1e06cb125cb53e5db21d872d27a
Peter <peterdotliu@gmail.com>
2020-03-27 23:34:10 -0400

    Format code via Prettier

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,54 +1,51 @@
-import React from 'react'
-import Country from './Country'
+import React from "react";
+import Country from "./Country";
 
-const Countries = (props) => {
-    const countries = props.countries.filter(c => c.name.toLowerCase().includes(props.search.toLowerCase().trim()))
+const Countries = props => {
+  const countries = props.countries.filter(c =>
+    c.name.toLowerCase().includes(props.search.toLowerCase().trim())
+  );
 
-    if(countries.length){
-        console.log(countries);
-        if(countries.length > 10){
-            console.log('Greater than 10');
-            return(
-                <p>Too many matches, specify another filter</p>
-            )
-        } else if(countries.length>1){
-            console.log('More than 1');
-            const showCountryNames = () => countries.map(c => 
-                <Country
-                details={false} 
-                key={c.numericCode} 
-                name={c.name} 
-                capital={c.capital}
-                pop={c.population}
-                languages={c.languages}
-                flag={c.flag}
-                />  
-            )
-            
-            return (
-                <div>
-                    <ul>
-                        {showCountryNames()}
-                    </ul>
-                </div>
-            )
-        } else {
-            return(
-                <Country
-                    details={true}
-                    name={countries[0].name} 
-                    capital={countries[0].capital}
-                    pop={countries[0].population}
-                    languages={countries[0].languages}
-                    flag={countries[0].flag}
-                />
-            )
-        }
+  if (countries.length) {
+    console.log(countries);
+    if (countries.length > 10) {
+      console.log("Greater than 10");
+      return <p>Too many matches, specify another filter</p>;
+    } else if (countries.length > 1) {
+      console.log("More than 1");
+      const showCountryNames = () =>
+        countries.map(c => (
+          <Country
+            details={false}
+            key={c.numericCode}
+            name={c.name}
+            capital={c.capital}
+            pop={c.population}
+            languages={c.languages}
+            flag={c.flag}
+          />
+        ));
+
+      return (
+        <div>
+          <ul>{showCountryNames()}</ul>
+        </div>
+      );
     } else {
-        return(
-            <p>No country data to show. Check api</p>
-        )
+      return (
+        <Country
+          details={true}
+          name={countries[0].name}
+          capital={countries[0].capital}
+          pop={countries[0].population}
+          languages={countries[0].languages}
+          flag={countries[0].flag}
+        />
+      );
     }
-}
+  } else {
+    return <p>No country data to show. Check api</p>;
+  }
+};
 
-export default Countries
+export default Countries;
--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,47 +1,54 @@
-import React, {useState, useEffect} from 'react'
-import Weather from './Weather'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import Weather from "./Weather";
+import axios from "axios";
 
-const Country = (props) => {
-    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${props.capital}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
-    const [details, setDetails] = useState(props.details)
-    const [weather, setWeather] = useState({})
+const Country = props => {
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${props.capital}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`;
+  const [details, setDetails] = useState(props.details);
+  const [weather, setWeather] = useState({});
 
-    useEffect(()=>{
-        axios
-        .get(apiURL)
-        .then(res => {
-         setWeather(res.data)
-        })
-        .catch(e => {
-            console.log(e)
-        })
-      },[apiURL])
+  useEffect(() => {
+    axios
+      .get(apiURL)
+      .then(res => {
+        setWeather(res.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }, [apiURL]);
 
-    const handleShow = () => {
-        setDetails(!details)
-    }
+  const handleShow = () => {
+    setDetails(!details);
+  };
 
-    if(!details){
-        return(
-            <li>{props.name}<button onClick={handleShow}>show</button></li>
-        )
-    } else {
-        return(
-            <div>
-            <h1>{props.name}<button onClick={handleShow}>show</button></h1>
-            <p>Capital: {props.capital}</p>
-            <p>Population: {props.pop}</p>
-            <h3>Languages</h3>
-            <ul>
-                {props.languages.map(l => <li key={l.name}>{l.name}</li>)}
-            </ul>
-            <img src={props.flag} alt={props.name}/>
-            <Weather weather={weather}/>
-            </div>
-            
-        )
-    }
-}
+  if (!details) {
+    return (
+      <li>
+        {props.name}
+        <button onClick={handleShow}>show</button>
+      </li>
+    );
+  } else {
+    return (
+      <div>
+        <h1>
+          {props.name}
+          <button onClick={handleShow}>show</button>
+        </h1>
+        <p>Capital: {props.capital}</p>
+        <p>Population: {props.pop}</p>
+        <h3>Languages</h3>
+        <ul>
+          {props.languages.map(l => (
+            <li key={l.name}>{l.name}</li>
+          ))}
+        </ul>
+        <img src={props.flag} alt={props.name} />
+        <Weather weather={weather} />
+      </div>
+    );
+  }
+};
 
-export default Country
+export default Country;
