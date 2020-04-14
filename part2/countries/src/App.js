9137a3f031bab9c06764a8305bbedeaf99df80f9
Peter <peterdotliu@gmail.com>
2020-04-14 13:17:45 -0400

    Update commits

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,35 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import Search from './components/Search'
-import Countries from './components/Countries'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Search from "./components/Search";
+import Countries from "./components/Countries";
+import axios from "axios";
 
 const App = () => {
-  const apiUrl = "https://restcountries.eu/rest/v2/all"
-  const [search, setSearch] = useState("")
-  const [countries, setCountries] = useState([])
+  const apiUrl = "https://restcountries.eu/rest/v2/all";
+  const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
 
   //effects
-  useEffect(()=>{
-    axios
-    .get(apiUrl)
-    .then(res => {
-     setCountries(res.data)
-    })
-  },[])
+  useEffect(() => {
+    axios.get(apiUrl).then(res => {
+      setCountries(res.data);
+    });
+  }, []);
 
   //event handlers
-  const handleSearch = (e) => {
-    setSearch(e.target.value)
-  }
+  const handleSearch = e => {
+    setSearch(e.target.value);
+  };
 
   return (
     <div>
-      <Search search={search} handler={handleSearch}/>
-      <Countries countries={countries} search={search}/>
+      <Search search={search} handler={handleSearch} />
+      <Countries countries={countries} search={search} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,40 +1,39 @@
 import React from "react";
 import serv from "../services/PersonsService";
 
-const PersonForm = props => {
+const PersonForm = (props) => {
   const allPersons = [...props.persons];
-  const addName = e => {
+  const addName = (e) => {
     e.preventDefault();
-    // const allNames = () => props.persons.map(p => p.name.toUpperCase())
-    // const allNum = () => props.persons.map(p => p.number)
     if (!props.newName.trim() || !props.newNumber.trim()) {
       window.alert("Text boxes cannot be empty");
     } else {
       const personObj = {
         name: props.newName,
-        number: props.newNumber
+        number: props.newNumber,
       };
-      if (allPersons.some(p => p.name === props.newName)) {
-        console.log(allPersons.findIndex(p => p.name === props.newName));
+      if (allPersons.some((p) => p.name === props.newName)) {
+        console.log(allPersons.findIndex((p) => p.name === props.newName));
         if (
           window.confirm(
             `${props.newName} already exists in the phonebook. Replace old number with new?`
           )
         ) {
           const id =
-            allPersons[allPersons.findIndex(p => p.name === props.newName)].id;
+            allPersons[allPersons.findIndex((p) => p.name === props.newName)]
+              .id;
           serv.update(id, personObj).then(props.updateList());
         }
-      } else if (allPersons.some(p => p.number === props.newNumber)) {
+      } else if (allPersons.some((p) => p.number === props.newNumber)) {
         window.alert(
           `${props.newNumber} already exists in the phonebook. Please add a different number`
         );
       } else {
         const msgObject = {
           type: "success",
-          msg: `Added ${props.newName}`
+          msg: `Added ${props.newName}`,
         };
-        serv.create(personObj).then(res => {
+        serv.create(personObj).then((res) => {
           props.setPersons(props.persons.concat(res.data));
           props.setNewName("");
           props.setNewNumber("");
