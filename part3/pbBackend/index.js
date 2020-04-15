a15a510db12e93626037b24bc6947b11822fbf5b
Peter <peterdotliu@gmail.com>
2020-04-15 14:28:19 -0400

    Submit 3.8: Phonebook backend step8

--- part3/pbBackend/index.js
+++ part3/pbBackend/index.js
@@ -22,7 +22,10 @@ let persons = [
 ];
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(":method :url :status :response-time ms :body"));
 
 app.get("/api/persons/", (req, res) => {
   res.json(persons);
