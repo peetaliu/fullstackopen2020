6dca6fd7bbb57d3eea8de8a3c90d6170ff9670ed
Peter <peterdotliu@gmail.com>
2020-04-15 13:25:31 -0400

    Submit 3.4: Phonebook backend step4

--- part3/pbBackend/index.js
+++ part3/pbBackend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
-const persons = [
+let persons = [
   {
     name: "Ada Lovelace",
     number: "39-44-5323523",
@@ -41,6 +41,12 @@ app.get("/api/persons/:id", (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
 });
 
 app.listen(PORT, () => {
