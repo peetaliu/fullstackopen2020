11344f293143b71426aa60bed89bd0f5d4481270
Peter <peterdotliu@gmail.com>
2020-05-08 16:36:31 -0400

    Lesson Material part4c: User administration

--- part3/lessons/models/note.js
+++ part3/lessons/models/note.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+mongoose.set('useFindAndModify', false)
+
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
@@ -8,11 +10,16 @@ const noteSchema = new mongoose.Schema({
   },
   date: Date,
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
 })
 
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
+
     delete returnedObject._id
     delete returnedObject.__v
   },
