126fe01be121b6a74abbe4bc46283f4b0940d77e
Peter <peterdotliu@gmail.com>
2020-05-19 20:27:34 -0400

    Submit 5.9 blog list frontend step9

--- part5/bloglist/client/src/App.js
+++ part5/bloglist/client/src/App.js
@@ -121,9 +121,11 @@ const App = () => {
       )}
 
       <h2>blogs</h2>
-      {blogs.map(blog => (
-        <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => (
+          <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
+        ))}
     </div>
   )
 }
