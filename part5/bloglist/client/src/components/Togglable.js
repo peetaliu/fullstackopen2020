92ccc768ce360f333c8501b218a1660ac3a1f186
Peter <peterdotliu@gmail.com>
2020-05-20 17:20:17 -0400

    Submit 5.11 Blog list frontend step11

--- part5/bloglist/client/src/components/Togglable.js
+++ part5/bloglist/client/src/components/Togglable.js
@@ -1,5 +1,5 @@
 import React, { useState, useImperativeHandle } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types' //Ex 5.11
 
 const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
