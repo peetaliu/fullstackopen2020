4b2bbdcd2c97d5ba48b8d25f55ba3a33136d9da9
Peter <peterdotliu@gmail.com>
2020-05-29 19:12:16 -0400

    Part 5-11

--- courseMaterial/noteApp/client/cypress/integration/note_app.spec.js
+++ courseMaterial/noteApp/client/cypress/integration/note_app.spec.js
@@ -1,6 +1,5 @@
 // Cypress tests use Mocha under the hood. As per Mocha docs,
 // avoid using arrow functions as they may cause issues in certain circumstances
-
 describe('Note app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
@@ -13,7 +12,23 @@ describe('Note app', function () {
     cy.visit('http://localhost:3000')
   })
 
-  it.only('login fails with wrong password', function () {
+  it('front page can be opened', function () {
+    cy.contains('Notes')
+    cy.contains(
+      'Note app, Department of Computer Science, University of Helsinki 2020'
+    )
+  })
+
+  it('user can log in', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('peetaliu')
+    cy.get('#password').type('password')
+    cy.get('#login-button').click()
+
+    cy.contains('Peter Liu logged in')
+  })
+
+  it('login fails with wrong password', function () {
     cy.contains('login').click()
     cy.get('#username').type('peetaliu')
     cy.get('#password').type('wrong')
@@ -23,54 +38,35 @@ describe('Note app', function () {
       .should('contain', 'Wrong credentials')
       .and('have.css', 'color', 'rgb(255, 0, 0)')
       .and('have.css', 'border-style', 'solid')
-  })
-  it('front page can be opened', function () {
-    cy.contains('Notes')
-    cy.contains(
-      'Note app, Department of Computer Science, University of Helsinki 2020'
-    )
-  })
-
-  it('login form can be opened', function () {
-    cy.contains('login').click()
-  })
-
-  it('user can login', function () {
-    cy.contains('login').click()
-    cy.get('#username').type('peetaliu')
-    cy.get('#password').type('password')
-    cy.get('#login-button').click()
-    cy.contains('Peter Liu logged in')
-    cy.contains('new note').click()
+    cy.get('html').should('not.contain', 'Peter Liu logged in')
   })
 
   describe('when logged in', function () {
     beforeEach(function () {
-      cy.contains('login').click()
-      cy.get('#username').type('peetaliu')
-      cy.get('#password').type('password')
-      cy.get('#login-button').click()
-      cy.contains('Peter Liu logged in')
+      cy.login({ username: 'peetaliu', password: 'password' })
     })
 
     it('a new note can be created', function () {
       cy.contains('new note').click()
       cy.get('input').type('a note created by cypress')
       cy.contains('save').click()
+
       cy.contains('a note created by cypress')
     })
 
     describe('and a note exists', function () {
       beforeEach(function () {
-        cy.contains('new note').click()
-        cy.get('input').type('another note cypress')
-        cy.contains('save').click()
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
       })
 
       it('it can be made important', function () {
-        cy.contains('another note cypress').contains('make important').click()
-
-        cy.contains('another note cypress').contains('make not important')
+        cy.contains('second note').parent().find('button').click()
+        cy.contains('second note')
+          .parent()
+          .find('button')
+          .should('contain', 'make not important')
       })
     })
   })
