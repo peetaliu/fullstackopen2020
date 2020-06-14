13716c0dafbe4521560276ebe47c5fdc245be603
Peter <peterdotliu@gmail.com>
2020-06-13 23:58:24 -0400

    Lesson Material Intro to redux devtools

--- courseMaterial/notes-redux/src/index.js
+++ courseMaterial/notes-redux/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import App from './App'
 
@@ -12,7 +13,7 @@ const reducer = combineReducers({
   filter: filterReducer,
 })
 
-const store = createStore(reducer)
+const store = createStore(reducer, composeWithDevTools())
 
 console.log(store.getState())
 
