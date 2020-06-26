aae62d3afcca9e8e212d7181fcafd73e9a1f70f0
Peter <peterdotliu@gmail.com>
2020-06-26 16:45:34 -0400

    Submit 7.7 country hook

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,47 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const useField = (type) => {
+const useField = type => {
   const [value, setValue] = useState('')
 
-  const onChange = (event) => {
+  const onChange = event => {
     setValue(event.target.value)
   }
 
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
-const useCountry = (name) => {
+const useCountry = name => {
   const [country, setCountry] = useState(null)
-
-  useEffect()
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+      .then(res => {
+        const c = res.data[0]
+        setCountry({
+          name: c.name,
+          capital: c.capital,
+          population: c.population,
+          flag: c.flag,
+          found: 1,
+        })
+        console.log('res.data', res.data)
+      })
+      .catch(e => {
+        console.log(e.message)
+        setCountry({ found: false })
+      })
+  }, [name])
 
   return country
 }
 
 const Country = ({ country }) => {
+  console.log('country', country)
   if (!country) {
     return null
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{country.name} </h3>
+      <div>capital {country.capital} </div>
+      <div>population {country.population}</div>
+      <img src={country.flag} height='100' alt={`flag of ${country.name}`} />
     </div>
   )
 }
@@ -51,7 +65,7 @@ const App = () => {
   const [name, setName] = useState('')
   const country = useCountry(name)
 
-  const fetch = (e) => {
+  const fetch = e => {
     e.preventDefault()
     setName(nameInput.value)
   }
@@ -63,7 +77,7 @@ const App = () => {
         <button>find</button>
       </form>
 
-      <Country country={country} />
+      {name ? <Country country={country} /> : null}
     </div>
   )
 }
